86e832bd1fbc1135d5ace4b818d63570c60284ae
Abhay Sharma <abhay.svce@gmail.com>
2024-10-14 16:01:23 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict" // Treat all js code as a new version
+
+// alert(2+2) // We are using node js, not browser
+
+console.log(3+3);
+ console.log("Abhay Sharma");
+
+ let name = "ABhay";
+ let age = 21;
+ let isLoggedIn = false;
+
+ // number => 2 to power 53
+//  bigInt
+// string ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "abhay");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);
+
+
+
+
+

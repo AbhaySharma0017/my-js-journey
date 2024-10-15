926e0799aef94f92e376e8efffab667d45316995
Abhay Sharma <abhay.svce@gmail.com>
2024-10-15 06:34:00 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,54 @@
+let score = 30;
+
+console.log(typeof score);
+console.log(typeof (score)); // As a method bhi we can check
+
+
+let maks = "30";
+console.log(typeof maks);
+let valueInNumber = Number(maks)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(maks);
+
+
+let level = "33abh";
+console.log(typeof level);
+console.log(level);
+let change = Number(level);
+console.log(typeof change);
+console.log(change);
+
+let score1 = null;
+console.log(score1);
+console.log(typeof score1);
+let change1 = Number(score1);
+console.log(typeof change1);
+console.log(change1);
+
+
+// "33" easily convert to => 33
+// "33abs" => Nan(not a number)
+//true, convert in 1 && false, convert in 0
+
+let logged = 1;
+let isLogged = "";
+let bool = Boolean(logged);
+let isIN = Boolean(isLogged);
+console.log(typeof isLogged);
+console.log(bool);
+console.log(typeof bool);
+console.log(typeof logged);
+console.log(typeof isIN);
+console.log(isIN);
+
+let sumNumber = 7489;
+let number = String(sumNumber);
+console.log(sumNumber);
+
+console.log(number);
+console.log(typeof number);
+
+
+
+

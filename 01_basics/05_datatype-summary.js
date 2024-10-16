a0018292b2a076f757347879cba70344fbd81ee9
Abhay Sharma <abhay.svce@gmail.com>
2024-10-16 09:12:16 +0000

    datType summary

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,56 @@
+/*
+   dataType :- There are 2 type of data type.
+    1) Primitive
+    2) Non-Primitive (Reference type)
+
+    1) Primitive type :- Threre are 7 type of primitive data type
+      String,Number,Boolean,null,undefiend,Symbol,BigInt.
+
+    2) Non-Primitive :- Array, object, function.
+
+*/
+
+// *Number datatype
+
+// const score = 100;
+// const score1 = 100.3;
+// console.log(typeof score, score );
+
+// *Boolean datatype
+
+// const isLogged = false;
+//  *null datatype
+// const temp = null;
+// console.log(typeof temp, typeof isLogged);
+
+//* Undefined
+
+// let userEmail;
+// let usermail = undefined;
+// console.log(userEmail, typeof usermail);
+
+// *Symbol 
+//  const id = Symbol('123');
+//  const anotherId = Symbol('123');
+//  console.log(id == anotherId);
+ 
+// * BigInt
+// const bigNumber = 214748362782713384798n;
+// console.log(bigNumber);
+
+// * Object
+// let obj = {
+//     name : 'ABhay',
+//     age : 21,
+//     Address : "Indore"
+// };
+// console.table(obj);
+
+// * Function
+
+function f1(){
+    console.log("Hey buddy !!"); 
+};
+console.log(f1());
+
+

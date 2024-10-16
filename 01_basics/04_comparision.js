098348682ef05ddf87fc340d2fbd73c1dcec171f
Abhay Sharma <abhay.svce@gmail.com>
2024-10-16 06:26:43 +0000

    Different type of comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,36 @@
+// console.log(2>1);
+// console.log(2>=2);
+// console.log(2!=2);
+// console.log(2==2);
+// console.log(2<3);
+
+// console.log("1" < 3); // 
+// console.log("02" > 1);
+// console.log("Abhay" > 2);
+// console.log("A" >= "A");
+// console.log("A" == "a");
+// console.log("A" == "A");
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log(undefined >= null);
+// console.log(undefined <= null);
+
+// === (Strictly check datatype)
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
+
+
+
+

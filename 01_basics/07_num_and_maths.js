0ed96aa64f4f29b0ef170cf7b8eb9aacb843fa30
Abhay Sharma <abhay.svce@gmail.com>
2024-10-20 05:58:42 +0000

    Number and Math

---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,27 @@
+// const num = 400;
+// console.log(num);
+// const num1 = new Number(100);
+// console.log(num1);
+
+// console.log(num1.toString());
+// console.log(num1.toString().length);
+// console.log(num1.toFixed(2));
+
+// const otherNum = 123.9281;
+// console.log(otherNum.toPrecision(4));
+
+// const num2 = 1000000;
+// console.log(num2.toLocaleString('en-IN')); // IN india
+
+//**********************************************-MATHS-*************************************************/
+console.log(Math);
+console.log(Math.abs(-6)); // only use to convert negative value to positive not vice versa
+// console.log(Math.round(6.6));
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+
